dd35290a5a25671180c8f5eb5e5574abb3d3fe7c
draguleee <andredragu1699@gmail.com>
2025-05-21 22:10:41 +0300

    Course 02

---
+++ Course2/InternetExplorer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D
+{
+    class InternetExplorer
+    {
+        // Class variables (attributes)
+        private int windowWidth;
+
+        // Constructor with parameters
+        public InternetExplorer(int windowWidth)
+        {
+            this.windowWidth = windowWidth;
+        }
+
+        // Getter for width
+        public void DisplayWindowWidth()
+        {
+            if (windowWidth > 1920)
+            {
+                int i = 0;
+                while (i < 3)
+                {
+                    Console.WriteLine("windowWidth = " + windowWidth);
+                    i++;
+                }
+            }
+            else
+            {
+                for (int i = 0; i < 2; i++)
+                {
+                    Console.WriteLine("windowWidth = " + windowWidth);
+                }
+            }
+        }
+    }
+}
---
+++ Course2/Test.cs
@@ -0,0 +1,57 @@
+﻿using B;
+using C;
+using D;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course2
+{
+    class Test
+    {
+        static void Main(string[] args)
+        {
+
+            /* FIREFOX */
+
+            // Create a Firefox object
+            Firefox firefox = new Firefox(1920);
+
+            // Convert the width to string
+            string widthString = firefox.ConvertWidthToString(firefox.GetWindowWidth());
+            double widthDouble = firefox.ConvertWidthToDouble(firefox.GetWindowWidth());
+            
+            // Print the converted values in the console
+            Console.WriteLine("width is of type " + firefox.GetWindowWidth().GetType().Name);
+            Console.WriteLine("widthString is of type " + widthString.GetType().Name);
+            Console.WriteLine("widthDouble is of type " + widthDouble.GetType().Name);
+
+
+            /* CHROME */
+
+            // Create Chrome objects
+            Chrome chrome1 = new Chrome(2048);
+            Chrome chrome2 = new Chrome(1080);
+
+            // Call DisplayWindowWidth() method for both objects
+            chrome1.DisplayWindowWidth();
+            chrome2.DisplayWindowWidth();
+
+
+            /** INTERNET EXPLORER */
+
+            // Create InternetExplorer objects
+            InternetExplorer ie1 = new InternetExplorer(4096);
+            InternetExplorer ie2 = new InternetExplorer(800);
+
+            // Call DisplayWindowWidth() method for both objects
+            ie1.DisplayWindowWidth();
+            ie2.DisplayWindowWidth();
+
+            Console.ReadKey();
+
+        }
+    }
+}
