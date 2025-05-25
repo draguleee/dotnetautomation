2122690b89bb4dc734646414508679151400a4fb
draguleee <andredragu1699@gmail.com>
2025-05-25 21:41:12 +0300

    Course 1

---
+++ Course 1/Browser.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course_1
+{
+    class Browser
+    {
+        // Class variables (attributes)
+        public int windowWidth;
+
+        // Default constructor
+        public Browser()
+        {
+            windowWidth = 1024;
+        }
+
+        // Constructor with parameters
+        public Browser(int windowWidth)
+        {
+            this.windowWidth = windowWidth;
+        }
+
+        // Method to change the window's width
+        public void ChangeWindowWidth(int windowWidth)
+        {
+            this.windowWidth = windowWidth;
+            Console.WriteLine("New window width: " + this.windowWidth);
+        }
+    }
+}
