d8c4d7419951af6d909cfe6f4830de545666647e
draguleee <andredragu1699@gmail.com>
2025-05-25 21:42:05 +0300

    Course 1 Homework

---
+++ Homework/HW_Course1/Browser.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_Course1
+{
+    class Browser
+    {
+        // Class variables
+        int height;
+        int width;
+
+        // Constructor with parameters
+        public Browser(int height, int width)
+        {
+            this.height = height;
+            this.width = width;
+        }
+
+        // Method to display the height 
+        public void DisplayWindowHeight()
+        {
+            Console.WriteLine("Window height: " + height);
+        }
+
+        // Method to display the width
+        public void DisplayWindowWidth()
+        {
+            Console.WriteLine("Window width: " + width);
+        }
+
+        // Method to change window height
+        public void ChangeWindowHeight(int height)
+        {
+            this.height = height;
+            Console.WriteLine("New window height: " + this.height);
+        }
+
+        // Method to change window width
+        public void ChangeWindowWidth(int width)
+        {
+            this.width = width;
+            Console.WriteLine("New window width: " + this.width);
+        }
+    }
+}
---
+++ Homework/HW_Course1/Test.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_Course1
+{
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            // Create a Browser object
+            Browser browser = new Browser(400, 600);
+
+            // Display the current window 
+            browser.DisplayWindowHeight();
+            browser.DisplayWindowWidth();
+
+            // Change the window height and width
+            browser.ChangeWindowHeight(1080);
+            browser.ChangeWindowWidth(1920);
+
+            Console.ReadKey();
+        }
+    }
+}
