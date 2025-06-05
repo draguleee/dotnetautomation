17f2455dbe30570f990628d80272da28eea0e67a
draguleee <andredragu1699@gmail.com>
2025-06-05 15:45:28 +0300

    Course 4

---
+++ Course 4/Course 4/Chrome.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course_4
+{
+    class Chrome : Browser, IComponent
+    {
+        // Provide implementation for the ChangeWindowWidth() method
+        public override void ChangeWindowWidth(int newWindowWidth)
+        {
+            int windowWidth = base.WindowWidth;
+            windowWidth = newWindowWidth;
+            Console.WriteLine("New window width for Chrome: " + windowWidth);
+        }
+
+        // Provide implementation for the About() method
+        public void About()
+        {
+            Console.WriteLine("This is the Chrome browser.");
+        }
+    }
+}
---
+++ Course 4/Course 4/Test.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course_4
+{
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            // Create Firefox object
+            Firefox firefox = new Firefox();
+            firefox.WindowWidth = 1920;
+            Console.WriteLine(firefox.WindowWidth);
+            // firefox.SetWindowWidth(1920);
+            // Console.WriteLine(firefox.GetWindowWidth());
+            firefox.About();
+
+            // Create Chrome object
+            Chrome chrome = new Chrome();
+            chrome.WindowWidth = 1080;
+            Console.WriteLine(chrome.WindowWidth);
+            // chrome.SetWindowWidth(1080);
+            // Console.WriteLine(chrome.GetWindowWidth());
+            chrome.About();
+
+            Console.ReadKey();
+        }
+    }
+}
