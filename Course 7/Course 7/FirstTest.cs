55ca138adbce4e926f5243686c1753b7982a1181
draguleee <andredragu1699@gmail.com>
2025-06-06 13:53:33 +0300

    Course 7

---
+++ Course 7/Course 7/FirstTest.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Legacy;
+
+namespace Course_7
+{
+    [TestFixture]
+    public class FirstTest
+    {
+        [Test]
+        public void FirstNunitTest()
+        {
+            Assert.That(true, Is.True, "This is the modern NUnit 4 way.");
+        }
+    }
+}
