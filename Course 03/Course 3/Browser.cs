7b6f880669cd7ccfc00a526ff560ec9bf86b34c3
draguleee <andredragu1699@gmail.com>
2025-06-10 12:19:44 +0300

    Course 3

---
+++ Course 03/Course 3/Browser.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course_3
+{
+    class Browser
+    {
+        // Class variables
+        public int windowWidth_int;
+        public string windowWidth_string;
+
+        // Method to change window width (int)
+        public virtual void ChangeWindowWidth(int windowWidth_int)
+        {
+            Console.WriteLine("Method to change the Browser's window width. To be overridden.");
+        }
+
+        // Method to change window width (string)
+        public virtual void ChangeWindowWidth(string windowWidth_string)
+        {
+            Console.WriteLine("Method to change the Browser's window width. To be overridden.");
+        }
+    }
+}
---
+++ Course 03/Course 3/InternetExplorer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course_3
+{
+    class InternetExplorer : Browser
+    {
+        // Overridden method (for int value)
+        public override void ChangeWindowWidth(int windowWidth_int)
+        {
+            base.windowWidth_int = windowWidth_int;
+            Console.WriteLine("New window width for Internet Explorer (int): " + windowWidth_int);
+        }
+
+        // Overridden method (for string value)
+        public override void ChangeWindowWidth(string windowWidth_string)
+        {
+            base.windowWidth_string = windowWidth_string;
+            Console.WriteLine("New window width for Internet Explorer (string): " + windowWidth_string);
+        }
+    }
+}
