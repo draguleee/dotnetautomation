c776c62eb53f8a67b4ae063f434a9d9d8d9cfd1b
draguleee <andredragu1699@gmail.com>
2025-06-10 12:20:00 +0300

    Course 4

---
+++ Course 04/Course 4/Browser.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course_4
+{
+    public abstract class Browser
+    {
+        // Class variables
+        private int windowWidth;
+
+        /** ACCESORS & MUTATORS
+        
+        // Accesor (Getter) for windowWidth
+        public int GetWindowWidth()
+        {
+            return windowWidth;
+        }
+
+        // Mutator (Setter) for windowWidth
+        public void SetWindowWidth(int windowWidth)
+        {
+            this.windowWidth = windowWidth;
+        }
+
+        */
+
+        // Encapsulation using properties
+        public int WindowWidth
+        {
+            get { return windowWidth; }
+            set { windowWidth = value; }
+        }
+
+        // Method to change window width
+        public abstract void ChangeWindowWidth(int windowWidth);
+
+    }
+}
