093ec14c91a0c61c6a3776bdbc40ee613c082a2b
draguleee <andredragu1699@gmail.com>
2025-06-13 17:02:52 +0300

    Course 13

---
+++ Course 13/FinalAssignmentPageObjects/PageObjects/HomePage.cs
@@ -0,0 +1,101 @@
+﻿using FinalAssignmentPageObjects.Base;
+using FinalAssignmentPageObjects.Resources;
+using NUnit.Framework;
+using OpenQA.Selenium;
+
+namespace FinalAssignmentPageObjects.PageObjects
+{
+    /// <summary>
+    /// HomPage - represents the home page of the application.
+    /// </summary>
+    public class HomePage : PageBase
+    {
+        /// <summary>
+        /// HomePage constructor - initializes the driver and waits for elements.
+        /// </summary>
+        /// <param name="driver"></param>
+        public HomePage(IWebDriver driver) : base(driver) { }
+
+        /// <summary>
+        /// CheckDefaultEmailPassword method - checks the default email and password in the form section.
+        /// </summary>
+        /// <returns></returns>
+        public HomePage CheckDefaultEmailPassword()
+        {
+            var form = Wait.Until(d => d.FindElement(By.Id(Home.Form)));
+
+            var emailText = form.FindElement(By.Id(Home.Email)).GetAttribute("value");
+            Assert.That(emailText, Is.EqualTo(Home.DefaultEmail), Errors.DefaultEmailError);
+
+            var passwordText = form.FindElement(By.Id(Home.Password)).GetAttribute("value");
+            Assert.That(passwordText, Is.EqualTo(Home.DefaultPassword), Errors.DefaultPasswordError);
+
+            return this;
+        }
+
+        /// <summary>
+        /// CheckLoginFields method - checks if the login fields are displayed on the home page.
+        /// </summary>
+        /// <returns></returns>
+        public HomePage CheckLoginFields()
+        {
+            var form = Wait.Until(d => d.FindElement(By.Id(Home.Form)));
+
+            var emailField = form.FindElement(By.Id(Home.Email));
+            Assert.That(emailField.Displayed, Is.True, Errors.EmailFieldError);
+
+            var passwordField = form.FindElement(By.Id(Home.Password));
+            Assert.That(passwordField.Displayed, Is.True, Errors.PasswordFieldError);
+
+            var loginButton = form.FindElement(By.XPath(Home.Login));
+            Assert.That(loginButton.Displayed, Is.True, Errors.LoginButtonError);
+
+            return this;
+        }
+
+        /// <summary>
+        /// LogIn method - logs the user into the dashboard.
+        /// </summary>
+        /// <param name="email"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        public HomePage LogIn(string email, string password)
+        {
+            var form = Wait.Until(d => d.FindElement(By.Id(Home.Form)));
+
+            var emailField = form.FindElement(By.Id(Home.Email));
+            var passwordField = form.FindElement(By.Id(Home.Password));
+            var loginButton = form.FindElement(By.TagName(Home.Login));
+
+            emailField.Clear();
+            passwordField.Clear();  
+
+            emailField.SendKeys(email);
+            passwordField.SendKeys(password);
+
+            loginButton.Click();
+
+            if (string.IsNullOrEmpty(email) || string.IsNullOrEmpty(password))
+            {
+                var errorMessage = form.FindElement(By.Id(Home.NullText));
+                Assert.That(errorMessage.Text, Is.EqualTo(Errors.NullEmailAddressError), Errors.NullEmailAddressError);
+            }
+            else if (!email.Contains("@"))
+            {
+                var errorMessage = form.FindElement(By.Id(Home.FormatText));
+                Assert.That(errorMessage.Text, Is.EqualTo(Errors.InvalidEmailFormatError), Errors.InvalidEmailFormatError);
+            }
+            else if (email != Home.DefaultEmail || password != Home.DefaultPassword)
+            {
+                var errorMessage = form.FindElement(By.Id(Home.InvalidText));
+                Assert.That(errorMessage.Text, Is.EqualTo(Errors.InvalidCredentialsError), Errors.InvalidCredentialsError);
+            } else
+            {
+                Wait.Until(d => d.Title.Contains("Dashboard"));
+                Assert.That(Driver.Title, Does.Contain("Dashboard"), Errors.LoginFailedError);
+            }
+
+            return this;
+        }
+    }
+}
---
+++ Course 13/FinalAssignmentTests/Tests/TestHomePage.cs
@@ -0,0 +1,85 @@
+﻿using FinalAssignmentTests.Base;
+using FinalAssignmentTests.Resources;
+using NUnit.Framework;
+
+namespace FinalAssignmentTests.Tests
+{
+    /// <summary>
+    /// TestHomePage - represents the test class for the home page.
+    /// </summary>
+    [TestFixture]
+    public class TestHomePage : TestBaseHome
+    {
+        /// <summary>
+        /// TestHeaderSection - tests the header section of the home page.
+        /// </summary>
+        [Test]
+        public void TestHeader()
+        {
+            HomePage.CheckHeader(Commons.Header, Commons.Logo, Home.FirstLink, Home.SecondLink);
+        }
+
+        /// <summary>
+        /// TestPageTitle - tests the page title of the home page.
+        /// </summary>
+        [Test]
+        public void TestTitle()
+        {
+            HomePage.CheckTitle("Home");
+        }
+
+        /// <summary>
+        /// TestHeadingSection - tests the heading section of the home page.
+        /// </summary>
+        [Test]
+        public void TestHeading()
+        {
+            HomePage.CheckHeading("Welcome"); 
+        }
+
+        /// <summary>
+        /// TestDefaultEmailPassword - tests the default email and password on the home page.   
+        /// </summary>
+        [Test]
+        public void TestDefaultEmailPassword()
+        {
+            HomePage.CheckDefaultEmailPassword();
+        }
+
+        /// <summary>
+        /// TestLoginFields - tests the login fields on the home page.
+        /// </summary>
+        [Test]
+        public void TestLoginFields()
+        {
+            HomePage.CheckLoginFields();
+        }
+
+        /// <summary>
+        /// TestLogIn - tests the login functionality on the home page.
+        /// </summary>
+        [Test]
+        public void TestLogIn()
+        {
+            HomePage.LogIn("test@example.com", "password123");
+        }
+
+        /// <summary>
+        /// TestWikiLink - tests the functionality of the wiki link.
+        /// </summary>
+        [Test]
+        public void TestLink()
+        {
+            HomePage.CheckLink(Commons.Header, Home.SecondLink, "Selenium");
+        }
+
+        /// <summary>
+        /// TestHeaderSection - tests the header section of the home page.
+        /// </summary>
+        [Test]
+        public void TestFooter()
+        {
+            HomePage.CheckFooter(Commons.Footer, Commons.Privacy, Commons.Terms, Commons.Contact);
+        }
+    }
+}
